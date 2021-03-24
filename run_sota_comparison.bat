3d0e5c0acd0fe2d2c4358b13609a679ce88f60cb
Hadar <hadarsi320@gmail.com>
2021-03-24 18:51:07 +0200

    deleting irelevant files

--- run_sota_comparison.bat
+++
@@ -1,1 +0,0 @@
-"C:\Program Files\R\R-4.0.4\bin\Rscript.exe" "C:\Users\hadar\RStudioProjects\ClusterICP\sota_comparison.R"
--- run_split_comparison.bat
+++
@@ -1,1 +0,0 @@
-"C:\Program Files\R\R-4.0.4\bin\Rscript.exe" "C:\Users\hadar\RStudioProjects\ClusterICP\split_comparison.R"
